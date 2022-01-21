41cd13b3692e76a3ed409ebf55bc5d6d0585afd8
simran-gera <simran99.gera@gmail.com>
2022-01-21 11:32:58 +0530

    forked the master branch

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from enum import unique
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import json, sys
 
 try:
@@ -24,9 +25,13 @@ except FileNotFoundError:
 app = Flask(__name__)
 app.config[
     "SQLALCHEMY_DATABASE_URI"
-] = f"postgresql://{config['DB']['DB_USER']}:{config['DB']['DB_PWD']}@0.0.0.0/eapr"
+] = f"postgresql://{config['DB']['DB_USER']}:{config['DB']['DB_PWD']}@localhost/eapr"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
 db = SQLAlchemy(app)
+
+migrate = Migrate()
+migrate.init_app(app, db)
 
 
 class userdata(db.Model):
--- scripts/start_server.sh
+++
@@ -1,5 +0,0 @@
-# cd /home/ubuntu/OpenEHR-Project
-# source eapr/bin/activate
-echo hi
-pwd
-python3 app.py
